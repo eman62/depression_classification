75d20715d619f717ba3da47bfd66a1a90a8cb986
Ibrahim Alqayyas <ibrahim.alqayyas@nezam.io>
2022-06-20 21:17:09 +0200

    :hammer: hotfix

--- lib/views/03_admin/users/friends_screen.dart
+++ lib/views/03_admin/users/friends_screen.dart
@@ -157,7 +157,8 @@ class FriendItem extends StatelessWidget {
 
 sendWhatsapp(String phone, name) async {
   var whatsApp = phone.replaceAll(' ', '');
-  var whatsAppURl_android = 'whatsapp://send?phone=$whatsApp&&text=Talk to your friend $name she is not OK.';
+  var whatsAppURl_android =
+      'whatsapp://send?phone=$whatsApp&&text=Talk to your friend $name , your friend is not OK.';
   var whatAppURL_ios = 'https://wa.me/$whatsApp';
   if (Platform.isIOS) {
     // for iOS phone only
